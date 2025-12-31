8162c0f2d35679664b4214040292d5cefba01df2
cjdulay <37643523+cjdulay@users.noreply.github.com>
2025-12-30 23:12:40 -0700

    Increment cache version to v11

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'yyc-wash-v10'; // Increment this every time you push
+const CACHE_NAME = 'yyc-wash-v11'; // Increment this every time you push
 const ASSETS = [
   './', 
   './index.html', 

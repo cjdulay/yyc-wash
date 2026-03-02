82bccb1b3ff1633da78636e00ad892cedc7e9879
cjdulay <37643523+cjdulay@users.noreply.github.com>
2026-03-02 16:58:16 -0700

    Increment cache version to v30
    Updated cache name for versioning.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'yyc-wash-v29'; // Increment this every time you push
+const CACHE_NAME = 'yyc-wash-v30'; // Increment this every time you push
 const ASSETS = [
   './', 
   './index.html', 
